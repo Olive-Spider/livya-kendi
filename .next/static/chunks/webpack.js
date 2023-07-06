bf40f1565c77bde034a99fbc76dfe5175c803bd5
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-06 16:48:22 +0300

    resume update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa23be2867f9adb74a17"; }
+/******/ 		__webpack_require__.h = function() { return "7462ef9afa654ec94fd6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.97595d48f233a3593a2c.hot-update.js
+++ .next/static/webpack/webpack.97595d48f233a3593a2c.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fa23be2867f9adb74a17"; }
+/******/ 	__webpack_require__.h = function() { return "7462ef9afa654ec94fd6"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
